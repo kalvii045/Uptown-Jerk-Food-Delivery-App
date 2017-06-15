cadb444dff9b4ea7c0cd7ab10d5e258844f8d45d
Khalid <khalidalvii32@gmail.com>
2017-06-15 16:01:44 -0400

    Main header view for website

---
+++ views/main_head.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML> 
+
+<html>
+    <head> 
+        <title> Uptown Jerk </title>
+        <link href="css/styles.css" rel="stylesheet" />
+        <meta content="initial-scale=1, width=device-width" name="viewport"/>
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/3/w3.css">
+        <script src="js/jquery-1.11.3.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    <body>
+        <style> #title_part1{background-color:#d2a679;width:100%;height:20%;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);} 
+        #home_page_buttons{position:relative;float:right;}
+        div{font-family:"Palatino Linotype", "Book Antiqua", Palatino, serif;}
+        </style>
+        
+        <div id='title_part1'><div style='font-size:35px;'><img alt="uptown_jerk" src="img/uptown_jerk.jpg" height="65" width="65"> UPTOWN JERK <div id='home_page_buttons'> 
+            <a href="uptown_link.php" id='buttons'> HOME </a> <a href='order.php' id='buttons'> ORDER & MENU </a>
+            </div> </div>GENUINE JAMAICAN DISHES! REAL FOOD, BLESSED PRICES!<br>
+        </div>
+        
+            
+        
