1a64eee08e05bb6f8ca9c3aab7c4e42910ec4fbf
Khalid <khalidalvii32@gmail.com>
2017-06-15 15:59:19 -0400

    Customer order view confirmation

---
+++ views/confirmation_page_view.php
@@ -0,0 +1,6 @@
+<style> #confirmation_message1{margin-left:auto;margin-right:auto;text-align:center;background:white;color:black;font-size:35px;} </style>
+<div id='confirmation_message1'>
+    
+    THANK YOU FOR YOUR ORDER! YOUR ORDER SHOULD ARRIVE WITHIN 40 MINUTES!
+    
+</div>
