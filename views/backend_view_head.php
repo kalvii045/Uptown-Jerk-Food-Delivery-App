f992f88f3cdd9b18501e22cc61b50b4419da21e9
Khalid <khalidalvii32@gmail.com>
2017-06-15 15:55:33 -0400

    Backend/Admin page header

---
+++ views/backend_view_head.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Backend admin page </title>
+        <meta content="initial-scale=1, width=device-width" name="viewport"/>
+        <link href='css/styles.css' rel='stylesheet'> 
+      <style> #backend_title{margin-left:auto;margin-right:auto;text-align:center;} #image{float:left;position:absolute;} </style>  
+      
+      <script src="js/jquery-1.11.3.min.js"></script>
+    </head>
+    
+    <body>
+        <div id='top_box'> <div id='image'> <img src='img/uptown_jerk.jpg' alt='uptown_jerk' height='55' width='55'> </div>
+    <div id='backend_title'><div> UPTOWN JERK ADMIN PAGE! </div> <div> Check order status, add new orders to menu, and find 
+    other inquiries. </div> </div> </div>
+    <br><br>
+    <div id='backend_title'> <ul> 
+        <a href='order_history.php' id='buttons'> CHECK ALL ORDERS </a>
+        <a href='new_items.php' id='buttons'> ADD NEW ITEMS! </a> 
+    </ul>  </div>
+    
